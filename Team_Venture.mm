3b51abcbe56b6538f6c23afd8587c2b2d4861b6f
Ali-zain99 <alizainsharif48@gmail.com>
2025-09-26 17:02:36 +0500

    Free mind code

---
+++ Team_Venture.mm
@@ -0,0 +1,109 @@
+<map version="1.0.1">
+  <node TEXT="Teamup Ventures">
+    <node TEXT="Home - 340B Price Guide">
+      <node TEXT="Links">
+        <node TEXT="https://teamupventures.com/"/>
+        <node TEXT="https://teamupventures.com/pakistan-story"/>
+        <node TEXT="https://teamupventures.com/scrapboard"/>
+        <node TEXT="https://teamupventures.com/terms-conditions"/>
+      </node>
+      <node TEXT="Forms">
+        <node TEXT="Form: Contact (POST)">
+          <node TEXT="Field: your-name (text)"/>
+          <node TEXT="Field: your-email (email)"/>
+          <node TEXT="Field: company (text)"/>
+          <node TEXT="Field: subject (text)"/>
+          <node TEXT="Field: your-message (text)"/>
+          <node TEXT="Button: GET IN TOUCH"/>
+        </node>
+      </node>
+    </node>
+    <node TEXT="Pakistan Story">
+      <node TEXT="https://teamupventures.com/pakistan-story"/>
+    </node>
+    <node TEXT="Scrapboard Articles">
+      <node TEXT="Bayfikr  a FinTech app for Overseas Pakistanis">  
+        <node TEXT="https://teamupventures.com/scrapboard/bayfikr-a-user-friendly-fin-tech-app-making-payments-in-pakistan-easier-for-overseas-pakistanis"/>
+      </node>
+      <node TEXT="Clicky.pk attracts $700,000 in Pre-Series A">
+        <node TEXT="https://teamupventures.com/scrapboard/clicky-pk-attracts-700000-in-pre-series-a-funding-round"/>
+      </node>
+      <node TEXT="Pakistani Startups raised $65M+ in 2020">
+        <node TEXT="https://teamupventures.com/scrapboard/despite-a-pandemic-pakistani-startups-had-their-best-year-yet-with-over-65-million-in-funding"/>
+      </node>
+      <node TEXT="SadaPay raises $7.2M seed">
+        <node TEXT="https://teamupventures.com/scrapboard/exclusive-pakistani-fintech-sadapay-raises-7-2-million-in-countrys-largest-seed-round"/>
+      </node>
+      <node TEXT="Celebrating failure in startup ecosystem">
+        <node TEXT="https://teamupventures.com/scrapboard/for-a-successful-startup-ecosystem-pakistan-needs-to-learn-to-celebrate-failure"/>
+      </node>
+      <node TEXT="HumWell – inclusive telehealth service">
+        <node TEXT="https://teamupventures.com/scrapboard/humwell-comes-to-rescue-with-an-all-inclusive-telehealth-service"/>
+      </node>
+      <node TEXT="Shopsy introduces voice search">
+        <node TEXT="https://teamupventures.com/scrapboard/islamabad-based-startup-called-shopsy-introduces-voice-search-for-its-customers"/>
+      </node>
+      <node TEXT="AI detecting Covid-19">
+        <node TEXT="https://teamupventures.com/scrapboard/local-startup-is-using-ai-to-detect-covid-19"/>
+      </node>
+      <node TEXT="NIC welcomes 10th cohort">
+        <node TEXT="https://teamupventures.com/scrapboard/nic-welcomes-its-10th-cohort-of-startups"/>
+      </node>
+      <node TEXT="Remotebase raises $1.4M">
+        <node TEXT="https://teamupventures.com/scrapboard/pak-tech-startup-remotebase-raises-1-4-million-led-by-indus-valley-capital"/>
+      </node>
+      <node TEXT="Growth funds for startups">
+        <node TEXT="https://teamupventures.com/scrapboard/pakistan-launches-growth-funds-for-startups"/>
+      </node>
+      <node TEXT="Edkasa raises $320K">
+        <node TEXT="https://teamupventures.com/scrapboard/pakistani-edtech-startup-edkasa-raises-320000-pre-seed-by-launching-an-exam-prep-app-for-students"/>
+      </node>
+      <node TEXT="PayPro receives USAID grant">
+        <node TEXT="https://teamupventures.com/scrapboard/pakistani-fintech-startup-called-paypro-receives-a-grant-of-rs-7-4-million-from-usaid-and-smea"/>
+      </node>
+      <node TEXT="Pakistani startups break 2020 records">
+        <node TEXT="https://teamupventures.com/scrapboard/pakistani-startups-break-records-in-2020-despite-global-economic-slowdown"/>
+      </node>
+      <node TEXT="E-commerce marketing tool launched">
+        <node TEXT="https://teamupventures.com/scrapboard/pakistans-first-e-commerce-marketing-tool-launched-by-pakistani-entrepreneur-to-make-e-commerce-more-competitive"/>
+      </node>
+      <node TEXT="Sehat Kahani raises $1M">
+        <node TEXT="https://teamupventures.com/scrapboard/pakistans-sehat-kahani-raises-1-million-pre-series-a-to-grow-its-telemedicine-network"/>
+      </node>
+      <node TEXT="Truck It In raises $1.5M">
+        <node TEXT="https://teamupventures.com/scrapboard/pakistans-truck-it-in-raises-1-5-million-pre-seed-for-its-trucking-marketplace">
+            <node TEXT="Screenshot Example">
+                <richcontent TYPE="NODE">
+                    <html>
+                    <body>
+                        <img src="screenshots/scrapboard_clicky-pk-attracts-700000-in-pre-series-a-funding-round.png" width="150" height="100"/>
+                    </body>
+                    </html>
+                </richcontent>
+            </node>
+        </node>
+      </node>
+      <node TEXT="Startup challenges">
+        <node TEXT="https://teamupventures.com/scrapboard/startup-challenges-the-way-out"/>
+      </node>
+      <node TEXT="Era of VCs boom of startups">
+        <node TEXT="https://teamupventures.com/scrapboard/the-era-of-vcs-and-boom-of-start-ups"/>
+      </node>
+       <node TEXT="Grocery stores go online in Covid">
+        <node TEXT="https://teamupventures.com/scrapboard/to-survive-pakistans-covid-pandemic-grocery-stores-go-online"/>
+      </node>
+    </node>
+
+   <!-- Example node with an image -->
+    <node TEXT="Screenshot Example">
+    <richcontent TYPE="NODE">
+        <html>
+        <body>
+            <img src="screenshots/scrapboard_clicky-pk-attracts-700000-in-pre-series-a-funding-round.png" width="150" height="100"/>
+        </body>
+        </html>
+    </richcontent>
+    </node>
+
+  </node>
+</map>
